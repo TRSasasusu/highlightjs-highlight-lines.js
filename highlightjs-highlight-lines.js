553c6f63dd98997b84addac2d03e83380912b627
TRSasasusu <17728957+TRSasasusu@users.noreply.github.com>
2019-02-24 12:06:59 +0900

    hljs.initHighlightLinesOnLoad() works after hljs.initHighlightingOnLoad().

--- highlightjs-highlight-lines.js
+++ highlightjs-highlight-lines.js
@@ -1,6 +1,9 @@
 (function(w, d) {
+    'use strict';
+
     if(w.hljs) {
         w.hljs.initHighlightLinesOnLoad = initHighlightLinesOnLoad;
+        w.hljs.highlightLinesCode = highlightLinesCode;
     }
 
     /*
@@ -15,21 +18,36 @@
     ]
     */
     function initHighlightLinesOnLoad(options) {
-        var codes = d.getElementsByClassName('hljs');
-        for(var i = 0; i < codes.length; ++i) {
-            codes[i].innerHTML = codes[i].innerHTML.replace(/[ \S]*\n/gm, function(match) {
-                //return '<div class="highlight-line">' + match.slice(0, -1) + '<br /></div>';
-                return '<div class="highlight-line">' + match + '</div>';
-            });
-
-            if(options[i] === undefined) {
-                continue;
+        function callHighlightLinesCode() {
+            var codes = d.getElementsByClassName('hljs');
+            for(var i = 0; i < codes.length; ++i) {
+                highlightLinesCode(codes[i], options[i]);
             }
-            var lines = codes[i].getElementsByClassName('highlight-line');
-            for(var option of options[i]) {
-                for(var j = option.start; j <= option.end; ++j) {
-                    lines[j].style.backgroundColor = option.color;
-                }
+        }
+
+        if(d.readyState !== 'loading') {
+            callHighlightLinesCode();
+        }
+        else {
+            w.addEventListener('DOMContentLoaded', function() {
+                callHighlightLinesCode();
+            });
+        }
+    }
+
+    function highlightLinesCode(code, options) {
+        code.innerHTML = code.innerHTML.replace(/[ \S]*\n/gm, function(match) {
+            //return '<div class="highlight-line">' + match.slice(0, -1) + '<br /></div>';
+            return '<div class="highlight-line">' + match + '</div>';
+        });
+
+        if(options === undefined) {
+            return;
+        }
+        var lines = code.getElementsByClassName('highlight-line');
+        for(var option of options) {
+            for(var j = option.start; j <= option.end; ++j) {
+                lines[j].style.backgroundColor = option.color;
             }
         }
     }
